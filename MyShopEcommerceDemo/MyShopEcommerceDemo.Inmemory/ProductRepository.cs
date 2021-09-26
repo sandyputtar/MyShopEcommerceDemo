f8a7b2f552e8ec628b10b9383837e4ab9e5d321a
sandyputtar <49281560+sandyputtar@users.noreply.github.com>
2021-09-26 23:34:48 +0530

    Model and Storage modification

---
+++ MyShopEcommerceDemo/MyShopEcommerceDemo.Inmemory/ProductRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShopEcommerceDemo.core.Models;
+
+namespace MyShopEcommerceDemo.Inmemory
+{
+    class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+
+        }
+
+        public void commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void update(Product product)
+        {
+            Product c = products.Find(p => p.id == product.id);
+            if (c != null)
+            {
+                c = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+
+        public Product find(string id)
+        {
+            Product c = products.Find(p => p.id == id);
+            if (c != null)
+            {
+                return c;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+         {
+            return products.AsQueryable();
+         }
+
+        public void delete(string id)
+        {
+            Product c = products.Find(p => p.id == id);
+            if (c != null)
+            {
+                products.Remove(c);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
---
+++ MyShopEcommerceDemo/MyShopEcommerceDemo.core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopEcommerceDemo.core.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string name { get; set; }
+        public string description { get; set; }
+        [Range(0,1000)]
+        public decimal price { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public Product()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+    }
+}
