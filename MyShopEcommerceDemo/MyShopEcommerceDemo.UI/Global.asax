cb25819fdea72afef8df94f17407a197bd7d17f4
sandyputtar <49281560+sandyputtar@users.noreply.github.com>
2021-09-26 20:22:03 +0530

    Basic Solution Setup

---
+++ MyShopEcommerceDemo/MyShopEcommerceDemo.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShopEcommerceDemo.UI.MvcApplication" Language="C#" %>
---
+++ MyShopEcommerceDemo/MyShopEcommerceDemo.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShopEcommerceDemo.UI.Startup))]
+namespace MyShopEcommerceDemo.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
